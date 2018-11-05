5073a244d971a214f58ad3c1b5df7f1a0205fb4b
Molly McCarron <mccarronmollye@gmail.com>
2018-11-05 14:23:47 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,4 +6,10 @@ end
 def array_with_two_elements
   array_with_two_elements = [1,2]
   @my_two_array = array_with_two_elements
-end 
+end
+
+def first_element
+  taylor_swift_array = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  @taylor_swift = taylor_swift_array
+  taylor_swift_array[0]
+end
