b4f262d9005fcc481c72e63e13c5a0e7c352f563
Molly McCarron <mccarronmollye@gmail.com>
2018-11-05 14:10:35 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,3 +2,8 @@ def instantiate_new_array
   instantiate_new_array = []
   @my_new_array = instantiate_new_array
 end
+
+def array_with_two_elements
+  array_with_two_elements = [1,2]
+  @my_two_array = array_with_two_elements
+end 
